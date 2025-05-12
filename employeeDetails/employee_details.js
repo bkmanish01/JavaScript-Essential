5ada1b02af4b5426152a5b49555bf6726d95b6e6
bkmanish01 <hmcmanish01@gmail.com>
2025-05-11 19:19:21 -0600

    added new features

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,8 +1,8 @@
 //initialize the employees array object
 const employees = [
-      { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-      { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-      { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
+      { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization: 'JavaScript' },
+      { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, specialization: 'Python' },
+      { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specialization: 'Java' },
       //... More employee records can be added here
     ];
 
@@ -31,3 +31,4 @@ function findEmployeeById(employeeId) {
         document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID';
     }
 }
+
