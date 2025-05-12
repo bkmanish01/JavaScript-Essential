795a58552f7d6ad18b621291428a5b52954b3bd3
bkmanish01 <hmcmanish01@gmail.com>
2025-05-12 09:38:03 -0600

    added delete function

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -29,7 +29,8 @@ function showbooks() {
         <p><strong>Author Name:</strong> ${book.authorName}</p>
         <p><strong>Book Description:</strong> ${book.bookDescription}</p>
         <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>
-        <button onclick="editbook(${index})">Edit</button>`
+        <button onclick="editbook(${index})">Edit</button>
+        <button onclick="deletebook(${index})">Delete</button>`
     );
     document.getElementById('books').innerHTML = booksDiv.join('');
 }
@@ -52,3 +53,9 @@ function editbook(index) {
     document.getElementById('bookDescription').value = '';
     document.getElementById('pagesNumber').value = '';
  }
+
+//create a deletebook function
+function deletebook(index) {
+    books.splice(index, 1);
+    showbooks(); 
+}
